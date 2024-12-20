a7b2d1469ee579965cdf65995af8b3988a7de6d3
othmane <soghriothmane@gmail.com>
2024-12-20 15:24:06 +0100

    create pages and routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import { BrowserRouter as Router ,Routes,Route, } from "react-router";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Offers from "./pages/Offers";
+import ForgotPassword from "./pages/ForgotPassword";
+
 
 function App() {
   return (
     <>
-     <h1>Hello worlds</h1>
+     <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/signin' element={<SignIn />} />
+        <Route path='/signUp' element={<SignUp />} />
+        <Route path='/offers' element={<Offers />} />
+        <Route path='/forgot-password' element={<ForgotPassword/>} />
+      
+
+      </Routes>
+     </Router>
     </>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      testings
+    </div>
+  )
+}
+
+export default Home
